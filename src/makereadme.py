673419612add0604a73b10a95bc32d7ed4e47c56
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-04-06 14:19:11 +0200

    added python script that takes the readme json data, sorts the repositories alphabetically, checks the urls and shows status icons dynamically

---
+++ src/makereadme.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import json
+import sys
+import os
+import operator
+import requests
+
+class ReadmeMaker:
+
+    def __init__(self):
+
+        self.infile = os.path.join(os.path.curdir, 'readme-data.json')
+        self.outfile = 'README.md'
+        self.repository = 'https://github.com/NLeSC/collab-demos'
+        self.data = None
+        self.showTableFooter = False
+
+    def readjson(self):
+        if os.path.isfile(self.infile):
+            with open(self.infile, 'r') as f:
+                try:
+                    self.data = json.load(f)
+                except Exception as e:
+                    print('Something went wrong when trying to parse the JSON file. Check if the file ' +
+                          'is JSON-compliant at "http://jsonlint.com/".')
+                    raise e
+
+    def writemarkdown(self):
+
+        def linkify(url):
+
+            if len(url) == 0:
+                return ':x:'
+            else:
+                if url[0] == '/':
+                    url = self.repository + url
+
+                print('checking "' + url + '"...')
+
+                # verify whether the provided url is valid
+                request = requests.get(url)
+                if request.status_code == 200:
+                    # all good, link is valid
+                    return '[:white_check_mark:](' + url + ')'
+                elif request.status_code == 403:
+                    # who knows what the status is, I don't have access!
+                    return '[:no_entry_sign:](' + url + ')'
+                elif request.status_code == 404:
+                    # fail
+                    return '[:x:](' + url + ')'
+                else:
+                    # something else is going on
+                    print('Unusual status code (' + str(request.status_code) + ').')
+                    self.showTableFooter = True
+                    return '[:warning:](' + url + ')(' + str(request.status_code) + ')'
+
+
+
+        tableHeader = '| Name | Demo | Document | Screencast | Code | Presentation\n' +\
+                      '| --- | --- | --- | --- | --- | --- |\n'
+        tabularFormat = '| %s | %s | %s | %s | %s | %s |\n'
+        tableFooter = '|  |  |  |  |  |  |\n' + \
+                      '| For HTTP codes, see [here](https://www.w3.org/Protocols/rfc26' +\
+                      '16/rfc2616-sec10.html) |  |  |  |  |  |\n'
+
+        with open(self.outfile, 'w') as f:
+            f.write(self.data['header-paragraphs'])
+            f.write(tableHeader)
+
+            # sort list of dictionaries by self.data['data']['name'] value:
+            self.data['data'].sort(key=operator.itemgetter('name'))
+
+            for entry in self.data['data']:
+                f.write(tabularFormat % (entry['name'],
+                                         linkify(entry['urls']['demo']),
+                                         linkify(entry['urls']['document']),
+                                         linkify(entry['urls']['screencast']),
+                                         linkify(entry['urls']['code']),
+                                         linkify(entry['urls']['presentation'])))
+            if self.showTableFooter:
+                f.write(tableFooter)
+
+
+if __name__ == '__main__':
+
+    readmeMaker = ReadmeMaker()
+    readmeMaker.readjson()
+    readmeMaker.writemarkdown()
