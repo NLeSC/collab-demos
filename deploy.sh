6232b14037337df97dd979497c71682bafbb75f9
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-04-20 17:53:34 +0200

    updated deploy script with git-lfs workaround

--- deploy.sh
+++ deploy.sh
@@ -19,12 +19,23 @@ git commit -m "travis-ci: automatically generated README.md"
 # push from the current repo's master branch to the remote
 # repo's master branch.
 
+# git lfs struggles with urls, apparently, so need to use named remotes
 
-# works, ask for username and password:
-# git push origin master
+if git remote | grep togithub > /dev/null; then
 
-git push https://github.com/jspaaks/collab-demos.git master
+    echo "remote 'togithub' exists. No need to add it."
 
+else
 
-# should work
-# git push "https://${GH_USER}:${GH_TOKEN}@${GH_REF}" master:master
+    # note: this adds plaintext credentials to the Travis'es .git config file
+    git remote add togithub https://${GH_USER}:${GH_TOKEN}@${GH_REPO}
+
+fi
+
+# use the named remote to push to. Also, make the command as quiet as possible
+# using '--quiet' and '> /dev/null 2>&1' to avoid accidentally outputting
+# credentials, which may end up in a log somewhere.
+git push --quiet togithub master:master > /dev/null 2>&1
+
+# delete the 'togithub' remote for *marginally* better security
+git remote rm togithub
