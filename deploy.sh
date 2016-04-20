daeccb7c16cef92e15a5d86ca3f1497489cc8ebe
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-04-20 17:12:21 +0200

    partial commit ...figuring out stupid deploy

--- deploy.sh
+++ deploy.sh
@@ -3,28 +3,11 @@
 # This file is based on the instructions I found here:
 # https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 
-set -e # Exit with nonzero exit code if anything fails
+# exit on any error
+set -o errexit
 
-# check that GH_USER has been set using Bash magic from
-# http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
-if [ -z "${GH_USER:+x}" ]; then
-    echo "GH_USER is unset. Aborting.";
-    exit 1;
-fi
-
-# check that GH_TOKEN has been set using Bash magic from
-# http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
-if [ -z "${GH_TOKEN:+x}" ]; then
-    echo "GH_TOKEN is unset. Aborting.";
-    exit 1;
-fi
-
-# check that GH_REPO has been set using Bash magic from
-# http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
-if [ -z "${GH_REPO:+x}" ]; then
-    echo "GH_REPO is unset. Aborting.";
-    exit 1;
-fi
+# disallow unset variables
+set -o nounset
 
 # call the python script that generates the readme.md
 python3 src/makereadme.py
@@ -33,8 +16,15 @@ python3 src/makereadme.py
 git add ./README.md
 git commit -m "travis-ci: automatically generated README.md"
 
-# Force push from the current repo's master branch to the remote
-# repo's master branch. We redirect any output to /dev/null to hide any
-# sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_USER}:${GH_TOKEN}@${GH_REPO}" master:master > /dev/null 2>&1
+# push from the current repo's master branch to the remote
+# repo's master branch.
 
+
+# works, ask for username and password:
+# git push origin master
+
+git push https://github.com/jspaaks/collab-demos.git master
+
+
+# should work
+# git push "https://${GH_USER}:${GH_TOKEN}@${GH_REF}" master:master
