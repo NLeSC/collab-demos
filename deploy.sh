ee93b979872a5ecd8f5c69b55ab946f2c1fa8659
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-04-21 11:37:10 +0200

    now using ssh github deploy key instead of personal access token (restricted access)

--- deploy.sh
+++ deploy.sh
@@ -9,37 +9,18 @@ set -o errexit
 # disallow unset variables
 set -o nounset
 
+# command to install dependencies
+pip install -r requirements.txt
+
+# Re-attach HEAD by checking out master
+git checkout master
+
 # call the python script that generates the readme.md
 python3 src/makereadme.py
 
-# tell git who is doing this commit
-git config user.email "j.spaaks@esciencecenter.nl"
-git config user.name "Travis CI"
-
 # commit the updated README.md
 git add ./README.md
-git commit -m "travis-ci: automatically generated README.md"
+git commit --allow-empty -m "$(git config user.name): automatically generated README.md"
 
-# push from the current repo's master branch to the remote
-# repo's master branch.
-
-# git lfs struggles with urls, apparently, so need to use named remotes
-
-if git remote | grep togithub > /dev/null; then
-
-    echo "remote 'togithub' exists. No need to add it."
-
-else
-
-    # note: this adds plaintext credentials to the Travis'es .git config file
-    git remote add togithub https://${GH_USER}:${GH_TOKEN}@${GH_REPO}
-
-fi
-
-# use the named remote to push to. Also, make the command as quiet as possible
-# using '--quiet' and '> /dev/null 2>&1' to avoid accidentally outputting
-# credentials, which may end up in a log somewhere.
-git push --quiet togithub master:master > /dev/null 2>&1
-
-# delete the 'togithub' remote for *marginally* better security
-git remote rm togithub
+#git push git@github.com:${TRAVIS_REPO_SLUG}.git $TRAVIS_BRANCH
+git push origin master:master
